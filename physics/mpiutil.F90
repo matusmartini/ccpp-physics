c410f2d53b10340ec221c7ec88b47b2a4d71d109
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-01-01 18:44:45 -0700

    Add physics/mpiutil.F90

---
+++ physics/mpiutil.F90
@@ -0,0 +1,163 @@
+module mpiutil
+
+  use iso_fortran_env, only : real32, real64
+  use mpi, only: MPI_SUCCESS, MPI_BCAST
+  use mpi, only: MPI_INTEGER, MPI_REAL, MPI_DOUBLE_PRECISION, MPI_LOGICAL
+
+  implicit none
+
+  private
+  public ccpp_bcast
+
+  interface ccpp_bcast
+     procedure :: bcast_i32d0
+     procedure :: bcast_i32d1
+     procedure :: bcast_i32d2
+     procedure :: bcast_i32d3
+     procedure :: bcast_r32d0
+     procedure :: bcast_r64d0
+     procedure :: bcast_r32d1
+     procedure :: bcast_r64d1
+     procedure :: bcast_r32d2
+     procedure :: bcast_r64d2
+     procedure :: bcast_r32d3
+     procedure :: bcast_r64d3
+     procedure :: bcast_ld0
+  end interface ccpp_bcast
+
+contains
+
+! Helper routines for MPI broadcasting
+
+   subroutine bcast_i32d0(arr, root, comm, ierr)
+      integer, intent(inout) :: arr
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, 1, MPI_INTEGER, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_i32d0")
+      end if
+   end subroutine bcast_i32d0
+
+   subroutine bcast_i32d1(arr, root, comm, ierr)
+      integer, intent(inout) :: arr(:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_INTEGER, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_i32d1")
+      end if
+   end subroutine bcast_i32d1
+
+   subroutine bcast_i32d2(arr, root, comm, ierr)
+      integer, intent(inout) :: arr(:,:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_INTEGER, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_i32d2")
+      end if
+   end subroutine bcast_i32d2
+
+   subroutine bcast_i32d3(arr, root, comm, ierr)
+      integer, intent(inout) :: arr(:,:,:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_INTEGER, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_i32d3")
+      end if
+   end subroutine bcast_i32d3
+
+   subroutine bcast_r32d0(arr, root, comm, ierr)
+      use mpi, only: MPI_SUCCESS, MPI_BCAST, MPI_REAL
+      real(kind=real32), intent(inout) :: arr
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, 1, MPI_REAL, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r32d0")
+      end if
+   end subroutine bcast_r32d0
+
+   subroutine bcast_r64d0(arr, root, comm, ierr)
+      real(kind=real64), intent(inout) :: arr
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, 1, MPI_DOUBLE_PRECISION, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r64d0")
+      end if
+   end subroutine bcast_r64d0
+
+   subroutine bcast_r32d1(arr, root, comm, ierr)
+      real(kind=real32), intent(inout) :: arr(:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_REAL, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r32d1")
+      end if
+   end subroutine bcast_r32d1
+
+   subroutine bcast_r64d1(arr, root, comm, ierr)
+      real(kind=real64), intent(inout) :: arr(:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_DOUBLE_PRECISION, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r64d1")
+      end if
+   end subroutine bcast_r64d1
+
+   subroutine bcast_r32d2(arr, root, comm, ierr)
+      real(kind=real32), intent(inout) :: arr(:,:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_REAL, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r32d2")
+      end if
+   end subroutine bcast_r32d2
+
+   subroutine bcast_r64d2(arr, root, comm, ierr)
+      real(kind=real64), intent(inout) :: arr(:,:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_DOUBLE_PRECISION, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r64d2")
+      end if
+   end subroutine bcast_r64d2
+
+   subroutine bcast_r32d3(arr, root, comm, ierr)
+      real(kind=real32), intent(inout) :: arr(:,:,:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_REAL, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r32d3")
+      end if
+   end subroutine bcast_r32d3
+
+   subroutine bcast_r64d3(arr, root, comm, ierr)
+      real(kind=real64), intent(inout) :: arr(:,:,:)
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, size(arr), MPI_DOUBLE_PRECISION, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_r64d3")
+      end if
+   end subroutine bcast_r64d3
+
+   subroutine bcast_ld0(arr, root, comm, ierr)
+      logical, intent(inout) :: arr
+      integer, intent(in) :: root, comm
+      integer, intent(out) :: ierr
+      call MPI_BCAST(arr, 1, MPI_LOGICAL, root, comm, ierr)
+      if (ierr/=MPI_SUCCESS) then
+         call ccpp_external_abort("mpiutil.F90:bcast_ld0")
+      end if
+   end subroutine bcast_ld0
+     
+end module mpiutil
