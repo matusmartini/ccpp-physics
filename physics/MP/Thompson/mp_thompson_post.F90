ac01657e4ea5b31570d1ef86f8a68664b4d3b64e
Matus Martini <martini.matus@gmail.com>
2025-10-03 23:31:46 +0000

    Bring pre-v7 updates to mpi_f08 to mp_thompson_post.F90

--- physics/MP/Thompson/mp_thompson_post.F90
+++ physics/MP/Thompson/mp_thompson_post.F90
@@ -4,10 +4,8 @@
 !>This module contain the post processing of Thompson microphysics
 module mp_thompson_post
 
+   use mpi_f08
    use machine, only : kind_phys
-#ifdef MPI
-      use mpi_f08
-#endif
 
    implicit none
 
@@ -73,7 +71,7 @@ contains
       real(kind_phys),                 intent(in)    :: ttendlim
       integer,                         intent(in)    :: kdt
       ! MPI information
-      type(MPI_Comm),   intent(in   ) :: mpicomm 
+      type(MPI_Comm),   intent(in   ) :: mpicomm
       integer,          intent(in   ) :: mpirank
       integer,          intent(in   ) :: mpiroot
       ! CCPP error handling
